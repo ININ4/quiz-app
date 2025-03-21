21ebffe985a6bab35dab58a0691b3fcc405f2a0f
[poe-cat] <[katarzyna.tramea@gmail.com]>
2025-03-21 16:18:08 +0100

    initial commit

---
+++ src/test/java/com/inin4/quizApp/QuizAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inin4.quizApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
