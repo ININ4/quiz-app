a47a4a60a3ad5cfa8422579802ebabad1e05c169
Katarzyna Szewiel <84228264+poe-cat@users.noreply.github.com>
2025-05-01 13:00:46 +0200

    integration with QuizService and ResultService + endpoints POST, GET

--- src/main/java/com/inin4/quizApp/controller/QuizController.java
+++ src/main/java/com/inin4/quizApp/controller/QuizController.java
@@ -1,26 +1,61 @@
 package com.inin4.quizApp.controller;
 
-import com.inin4.quizApp.model.Question;
+import com.inin4.quizApp.model.*;
+import com.inin4.quizApp.service.QuizService;
+import com.inin4.quizApp.service.ResultService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/quiz")
 public class QuizController {
 
-    @GetMapping("/question")
-    public Question getSampleQuestion() {
+    private final QuizService quizService;
+    private final ResultService resultService;
 
-        return new Question(
-                "q1",
-                "What is the output of System.out.println(\"Hello World\")?",
-                Map.of(
-                        "A", "Hello World",
-                        "B", "hello world",
-                        "C", "Syntax Error"
-                ),
-                "A"
-        );
+    public QuizController(QuizService quizService, ResultService resultService) {
+        this.quizService = quizService;
+        this.resultService = resultService;
     }
+
+
+    @GetMapping("/all")
+    public List<Question> getAllQuestions() {
+        return quizService.getAllQuestions();
+    }
+
+    @GetMapping("/question/{id}")
+    public Question getQuestionById(@PathVariable String id) {
+        return quizService.getQuestionById(id)
+                .orElseThrow(() -> new RuntimeException("Question not found"));
+    }
+
+    @PostMapping("/answer")
+    public AnswerResponse submitAnswer(@RequestBody AnswerRequest request) {
+        boolean isCorrect = quizService.checkAnswer(request.getQuestionId(), request.getSelectedAnswer());
+        String correct = quizService.getQuestionById(request.getQuestionId())
+                .map(Question::getCorrectAnswer)
+                .orElse("N/A");
+        return new AnswerResponse(isCorrect, correct);
+    }
+
+    @GetMapping("/count")
+    public int totalQuestions() {
+        return quizService.getTotalQuestions();
+    }
+
+    @PostMapping("/result")
+    public void submitResult(@RequestBody Result result) {
+        result.setTimestamp(LocalDateTime.now());
+        resultService.saveResult(result);
+    }
+
+    @GetMapping("/results")
+    public List<Result> getResults() {
+        return resultService.getAllResults();
+    }
+
 }
+
