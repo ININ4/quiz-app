0c3c9b74d762e0105b21f89e4b2c276218ccb517
Katarzyna Szewiel <84228264+poe-cat@users.noreply.github.com>
2025-05-01 13:11:25 +0200

    Added default constructor required for Jackson deserialization

--- src/main/java/com/inin4/quizApp/model/Question.java
+++ src/main/java/com/inin4/quizApp/model/Question.java
@@ -6,8 +6,12 @@ public class Question {
 
     private String id;
     private String question;
-    private Map<String, String> options; //odpowiedzi do wyboru
+    private Map<String, String> options;
     private String correctAnswer;
+
+    public Question() {
+        // wymagany przez Jacksona
+    }
 
     public Question(String id, String question, Map<String, String> options, String correctAnswer) {
         this.id = id;
@@ -15,14 +19,17 @@ public class Question {
         this.options = options;
         this.correctAnswer = correctAnswer;
     }
-
+    
     public String getId() { return id; }
-    public String getQuestion() { return question; }
-    public Map<String, String> getOptions() { return options; }
-    public String getCorrectAnswer() { return correctAnswer; }
-
     public void setId(String id) { this.id = id; }
+
+    public String getQuestion() { return question; }
     public void setQuestion(String question) { this.question = question; }
+
+    public Map<String, String> getOptions() { return options; }
     public void setOptions(Map<String, String> options) { this.options = options; }
+
+    public String getCorrectAnswer() { return correctAnswer; }
     public void setCorrectAnswer(String correctAnswer) { this.correctAnswer = correctAnswer; }
 }
+
